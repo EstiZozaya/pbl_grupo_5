ec2399b96f11066135a44ebdcdb0b83a3fa04b47
EstiZozaya <estibaliz.zozaya@alumni.mondragon.edu>
2024-05-22 17:34:30 +0200

    Caracteristicas de detección con todas las fotos

--- Algoritmo/Deteccion/caracteristicas_deteccion.m
+++ Algoritmo/Deteccion/caracteristicas_deteccion.m
@@ -38,43 +38,7 @@ nitidez_bordeB = zeros(n, 1);
 filtro_laplace=[1, 1, 1; 1, -8, 1; 1, 1, 1];
 
 for i=1:n
-I = imread(T_buenacalidad_revisadas.image{i});
-I_gray = rgb2gray(I);
-
-I_borde = imbinarize(I_gray, 0.1);
-
-% quitar los puntos blancos que aparecen fuera del ciruclo
-se = strel('disk', 15);
-I_borde = imerode(I_borde, se);
-
-I = im2double(I);
-I(:, :, 1) = I(:, :, 1) .* I_borde;
-I(:, :, 2) = I(:, :, 2) .* I_borde;
-I(:, :, 3) = I(:, :, 3) .* I_borde;
-
-I = im2uint8(I);
-
-% OSCURECER BORDES 
-se = strel('disk', 100); 
-bordes = imerode(I_borde, se); 
-I_gray_oscura = I_gray * 0;
-I_gray_oscura(bordes) = I_gray(bordes);
-
-I_gray_oscura = adapthisteq(I_gray_oscura);
-
-[max_fila, max_columna] = find(I_gray_oscura == max(I_gray_oscura(:)));
-centroide_x = mean(max_fila);
-centroide_y = mean(max_columna);
-
-s = size(I_gray_oscura);
-tamano_roi = min(s)/2;
-
-inicio_x = max(1, round(centroide_x - tamano_roi/2));
-fin_x = min(size(I_gray, 1), round(centroide_x + tamano_roi/2));
-inicio_y = max(1, round(centroide_y - tamano_roi/2));
-fin_y = min(size(I_gray, 2), round(centroide_y + tamano_roi/2));
-
-roi = I(inicio_x:fin_x, inicio_y:fin_y, :);
+roi = imread(['ROI', T_buenacalidad_revisadas.image{i}]);
 
 red_channel = roi(:, :, 1);
 green_channel = roi(:, :, 2);
