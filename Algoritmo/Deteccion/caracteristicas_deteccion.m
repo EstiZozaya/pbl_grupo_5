577ee4bf72a02472537baff9567c81aa82b4a284
EstiZozaya <estibaliz.zozaya@alumni.mondragon.edu>
2024-05-29 10:53:59 +0200

    Modelo con las que segmentan bien

--- Algoritmo/Deteccion/caracteristicas_deteccion.m
+++ Algoritmo/Deteccion/caracteristicas_deteccion.m
@@ -1,4 +1,5 @@
 close all; clc; clearvars;
+
 T_buenacalidad_revisadas = readtable('metadataCALIDADCORRECTA.csv');
 [n, m] = size(T_buenacalidad_revisadas);
 
@@ -38,115 +39,9 @@ nitidez_bordeB = zeros(n, 1);
 filtro_laplace=[1, 1, 1; 1, -8, 1; 1, 1, 1];
 
 for i=1:n
+copa = imread(['COPA', T_buenacalidad_revisadas.image{i}]);
+disco = imread(['DISCO2', T_buenacalidad_revisadas.image{i}]);
 roi = imread(['ROI', T_buenacalidad_revisadas.image{i}]);
-
-red_channel = roi(:, :, 1);
-green_channel = roi(:, :, 2);
-blue_channel = roi(:, :, 3);
-I_gray = rgb2gray(roi);
-
-se = strel('disk', 30); 
-
-canal_verde_sin_vasos = imclose(green_channel, se);
-canal_rojo_sin_vasos = imclose(red_channel, se); %el canal rojo no tiene vasos
-canal_azul_sin_vasos = imclose(blue_channel, se);
-gray_sin_vasos = imclose (I_gray, se);
-
-gray_sin_vasos1 = adapthisteq(gray_sin_vasos);
-canal_azul_sin_vasos1 = adapthisteq(canal_azul_sin_vasos);
-canal_verde_sin_vasos1 = adapthisteq(canal_verde_sin_vasos);
-canal_rojo_sin_vasos1 = adapthisteq(canal_rojo_sin_vasos);
-
-gray_sin_vasos2 = histeq(gray_sin_vasos);
-canal_azul_sin_vasos2 = histeq(canal_azul_sin_vasos);
-canal_verde_sin_vasos2 = histeq(canal_verde_sin_vasos);
-canal_rojo_sin_vasos2 = histeq(canal_rojo_sin_vasos);
-
-e_gris = entropy(I_gray);
-e_rojo = entropy(red_channel); % si es mas de 6 bien
-e_verde = entropy(green_channel);
-e_azul = entropy(blue_channel); % si es menos de 4 -> no usar %a partir de 6 no usar
-
-e_gris1 = entropy(gray_sin_vasos1);
-e_rojo1 = entropy(canal_rojo_sin_vasos1); 
-e_verde1 = entropy(canal_verde_sin_vasos1);
-e_azul1 = entropy(canal_azul_sin_vasos1);
-
-e_gris2 = entropy(gray_sin_vasos2);
-e_rojo2 = entropy(canal_rojo_sin_vasos2); 
-e_verde2 = entropy(canal_verde_sin_vasos2);
-e_azul2 = entropy(canal_azul_sin_vasos2);
-
-if e_gris1 > 5 
-    cup_threshold = 0.85 * max(gray_sin_vasos1(:)); % Umbral para la copa (brillo)
-    
-    cup_binary = gray_sin_vasos1 > cup_threshold; % Segmentación de la copa
-else 
-    cup_binary = ones(size(gray_sin_vasos));
-end
-
-if e_gris2 > 5 
-    disc_threshold = 0.9 * max(gray_sin_vasos2(:)); % Umbral para el disco completo (menos brillo)
-
-    disc_binary = gray_sin_vasos2 > disc_threshold; % Segmentación del disco completo
-else 
-    disc_binary = ones(size(gray_sin_vasos));
-end
-
-if e_rojo2 > 6
-    disc_thresholdR = 0.95 * max(canal_rojo_sin_vasos2(:)); % Umbral para el disco completo (menos brillo)
-
-    disc_binaryR = canal_rojo_sin_vasos2 > disc_thresholdR; % Segmentación del disco completo
-else 
-    disc_binaryR = ones(size(gray_sin_vasos));
-end
-
-if e_verde2 > 5
-    disc_thresholdG = 0.9 * max(canal_verde_sin_vasos2(:)); % Umbral para el disco completo (menos brillo)
-    
-    disc_binaryG = canal_verde_sin_vasos2 > disc_thresholdG; % Segmentación del disco completo
-else 
-    disc_binaryG = ones(size(gray_sin_vasos));
-end
- 
-if e_verde1 > 4.5 
-    cup_thresholdG = 0.95 * max(canal_verde_sin_vasos1(:)); % Umbral para la copa (brillo)
-  
-    cup_binaryG = canal_verde_sin_vasos1 > cup_thresholdG; % Segmentación de la copa
-else 
-    cup_binaryG = ones(size(gray_sin_vasos));
-end
-
-if e_azul1 > 5.5 
-    cup_thresholdB = 0.8 * max(canal_azul_sin_vasos1(:)); % Umbral para la copa (brillo)
-
-    cup_binaryB = canal_azul_sin_vasos1 > cup_thresholdB; % Segmentación de la copa
-else 
-    cup_binaryB = ones(size(gray_sin_vasos));
-end
-
-if e_azul2 > 4 
-    disc_thresholdB = 0.9 * max(canal_azul_sin_vasos2(:)); % Umbral para el disco completo (menos brillo)
-    
-    disc_binaryB = canal_azul_sin_vasos2 > disc_thresholdB; % Segmentación del disco completo
-else 
-    disc_binaryB = ones(size(gray_sin_vasos));
-end
-
-% Operación lógica de intersección para la segmentación de la copa
-cup_binary_comun = cup_binary & cup_binaryG & cup_binaryB;
-
-se = strel('disk', 10);
-disc_binaryR = imdilate(disc_binaryR, se);
-% Operación lógica de intersección para la segmentación del disco
-disc_binary_comun = disc_binary & disc_binaryR & disc_binaryG  & disc_binaryB;
-
-se = strel('disk', 15);
-disco = imerode(disc_binary_comun, se);
-disco = bwareafilt(disco, 1);
-disco = imdilate(disco, se);
-copa = bwareafilt(cup_binary_comun, 1);
-copa = imdilate(copa, se);
 
 [filaD, columnaD] = find(disco == max(disco(:)));
 % Calcula el centroide
@@ -156,7 +51,7 @@ centro_disco = [centroide_x, centroide_y];
 % Calcula el radio
 ancho_maximo = max(filaD) - min(filaD);
 alto_maximo = max(columnaD) - min(columnaD);
-radio_disco = max(ancho_maximo, alto_maximo) / 2;
+radio_disco = max(ancho_maximo, alto_maximo) / 2; 
 
 [filaC, columnaC] = find(copa == max(copa(:)));
 % Calcula el centroide
